121bb91486253155e0278d168eed49628f35a353
cmbaseball94 <Chris@Chriss-MacBook-Pro-3.local>
2015-01-19 13:48:09 -0500

    Final Touches Library
    Library Final Touches

--- Week 2/library/library.py
+++ Week 2/library/library.py
@@ -1,20 +1,20 @@
 __author__ = 'Chris'
 
 class Library: # Library Class
-    def __init__(self): #define the function
-        self.student = ""
-        self.grade_one = 0
-        self.grade_two = 0
-        self.grade_three = 0
-        self.total = 0
+    def __init__(self): # define the function
+        self.student = "" # Student Name
+        self.grade_one = 0 # Student Grade 1
+        self.grade_two = 0 # Student Grade 2
+        self.grade_three = 0 # Student Grade 3
+        self.total = 0 # Total of grades
 
     def mean(self,x,y,z): # Define the mean function
-        total = x + y + z
+        total = x + y + z # Adding Grades
         return total /3 # divide the total by 3
 
 
 class Printer(): # Printer Class
-    def print_out(self,name,avg):
+    def print_out(self,name,avg): # Print Out Function
         print "Hello "+ name + " your average is " + str(avg) # Printing the name and average
 
 
